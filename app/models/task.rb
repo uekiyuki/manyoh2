1c4a2cefc1578fac0ba4e54114151f576e216523
yuki ueki <karoo4413@gmail.com>
2020-01-09 10:19:28 +0900

    STEP2 降順テスト実装

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,5 @@
 class Task < ApplicationRecord
+  default_scope -> { order(created_at: :desc) }
+  validates :title, presence: true
+  validates :content, presence: true
 end
--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -1,18 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe 'タスク管理機能', type: :system do
+  before do
+    # 「タスク一覧画面」や「タスク詳細画面」などそれぞれのテストケースで、before内のコードが実行される
+    # 各テストで使用するタスクを1件作成する
+    # 作成したタスクオブジェクトを各テストケースで呼び出せるようにインスタンス変数に代入
+    # あらかじめタスク一覧のテストで使用するためのタスクを二つ作成する
+    FactoryBot.create(:task)
+    FactoryBot.create(:second_task)
+  end
+
   describe 'タスク一覧画面' do
     context 'タスクを作成した場合' do
       # テストコードを it '~' do end ブロックの中に記載する
       it '作成済みのタスクが表示されること' do
         # テストで使用するためのタスクを作成
-        task = FactoryBot.create(:task, title: 'task')
         # タスク一覧ページに遷移
         visit tasks_path 
         # visitした（遷移した）page（タスク一覧ページ）に「task」という文字列が
         # have_contentされているか？（含まれているか？）ということをexpectする（確認・期待する）
-        expect(page).to have_content 'task'
+        expect(page).to have_content 'Factoryで作ったデフォルトのタイトル１'
         # expectの結果が true ならテスト成功、false なら失敗として結果が出力される
+      end
+    end
+
+    context '複数のタスクを作成した場合' do
+      it 'タスクが作成日時の降順に並んでいること' do
+        new_task = FactoryBot.create(:task, title: 'new_task', content: 'new_task1' )
+        visit tasks_path
+        task_list = Task.all 
+        # byebug
+        # expect(page).to have_text /.*new_task.*task.*/m
+        expect(task_list[0].content).to eq 'new_task1'
+        # expect(Task.order("updated_at DESC").map(&:id))
+        save_and_open_page  
+        
+        # 省略
       end
     end
   end
@@ -33,11 +56,10 @@ RSpec.describe 'タスク管理機能', type: :system do
       # find(".csstask_content").set("test_content")
       # # 「登録する」というvalue（表記文字）のあるボタンをclick_onする（クリックする）
       # # 4.「登録する」というvalue（表記文字）のあるボタンをclick_onする（クリックする）する処理を書く
-      click_on 'Create Task'
+      click_on '登録する'
       # # clickで登録されたはずの情報が、タスク詳細ページに表示されているかを確認する
       # # （タスクが登録されたらタスク詳細画面に遷移されるという前提）
       # # 5.タスク詳細ページに、テストコードで作成したはずのデータ（記述）がhave_contentされているか（含まれているか）を確認（期待）するコードを書く
-      
       expect(page).to have_content 'test_title1'
       expect(page).to have_content 'test_content1'
       end
@@ -47,11 +69,8 @@ RSpec.describe 'タスク管理機能', type: :system do
   describe 'タスク詳細画面' do
     context '任意のタスク詳細画面に遷移した場合' do
       it '該当タスクの内容が表示されたページに遷移すること' do
-      
         @task1 = Task.create(title: "title01", content: "content01")
-
         visit task_path(@task1)
-        save_and_open_page
         # page.all("詳細を確認する")[2].click_link['2']
         expect(page).to have_content 'title01'
         expect(page).to have_content 'content01'
