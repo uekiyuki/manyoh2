722cc2514eec1331ab8a05537aa148b5c4ad6e2b
yuki ueki <karoo4413@gmail.com>
2020-01-09 15:38:00 +0900

    git push herokuエラーを修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root 'tasks#index'
   resources :tasks do
   end
-  
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
