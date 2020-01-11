c515631d93a8f1067b870f8bba850691f9f59d9b
yuki ueki <karoo4413@gmail.com>
2020-01-11 18:20:13 +0900

    終了期限及びソート機能追加

---
+++ db/migrate/20200111082826_add_status_to_tasks.rb
@@ -0,0 +1,6 @@
+class AddStatusToTasks < ActiveRecord::Migration[5.2]
+  def change
+    add_column :tasks, :status, :string, default: "未着手"
+    remove_column :tasks, :completed, :integer
+  end
+end
