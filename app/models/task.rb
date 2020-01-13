8ed6d8672ad623089fdedb1beaf75a0edcf045ff
yuki ueki <karoo4413@gmail.com>
2020-01-13 11:53:58 +0900

    検索機能追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,10 @@ class Task < ApplicationRecord
   # default_scope -> { order(created_at: :desc) }
   validates :title, presence: true
   validates :content, presence: true
+  scope :latest, -> {order(created_at: :desc)}
+  scope :expired, -> {order(time_limit: :asc)}
+  scope :search, -> (params) {where('(title LIKE ?) AND (status LIKE ?)',
+                                  "%#{params[:title]}%",
+                                  "%#{params[:status]}%")}
+
 end
