a5dfc84bb6008d5a913254fdd03f682c17eced41
yuki ueki <karoo4413@gmail.com>
2020-01-14 12:02:08 +0900

    テストでselectボタンが押せない

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,9 +4,11 @@ class Task < ApplicationRecord
   validates :content, presence: true
   scope :latest, -> {order(created_at: :desc)}
   scope :expired, -> {order(time_limit: :asc)}
-  scope :search, -> (params) {where('(title LIKE ?) AND (status LIKE ?)',
+  scope :search, -> (params) {
+  where('(title LIKE ?) AND (status LIKE ?)',
                                   "%#{params[:title_key]}%",
                                   "%#{params[:status_key]}%")}
+  
   scope :priority, -> {order(priority: :asc)}
   enum priority: { high: 0, medium: 1, low: 2 }
 
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -12,14 +12,28 @@ RSpec.describe 'タスク管理機能', type: :model do
   end
 
   it 'contentが空ならバリデーションが通らない' do
-    # ここに内容を記載する
     task = Task.new(title: '失敗テスト2', content: '')
     expect(task).not_to be_valid
   end
 
   it 'titleとcontentに内容が記載されていればバリデーションが通る' do
-    # ここに内容を記載する
     task = Task.new(title: '成功テスト', content: '成功テスト')
     expect(task).to be_valid
   end
+
+  it "検索ロジック" do
+    FactoryBot.create(:task)
+    FactoryBot.create(:second_task)
+    FactoryBot.create(:third_task)
+    FactoryBot.create(:task, status: "未着手")
+    FactoryBot.create(:second_task, status: "未着手")
+    expect_task = FactoryBot.create(:third_task, status: "完了")
+    result = Task.search(title_key: "タイトル3", status_key: "完了")
+    # binding.irb
+    expect(result[0].id).to be expect_task.id
+    expect(result.size).to eq 1
+  end
 end
+
+# バリデーションにmodelのテストを追加してみましょう。
+# 検索ロジックのmodelのテストを追加してみましょう。
