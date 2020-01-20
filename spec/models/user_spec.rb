e03f6d763f59681ae21ccca038a1e2c14616d4b2
yuki ueki <karoo4413@gmail.com>
2020-01-20 23:40:33 +0900

    user:modelテスト追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,24 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-  has_secure_password
-end
+  RSpec.describe User, type: :model do
+    it "名前が空ならバリデーションが通らない" do
+      user = User.new(name: '', email: 'a@example.com', password_digest: 'password')
+      expect(user).not_to be_valid
+    end
+  
+    it "メールアドレスが空ならバリデーションが通らない" do
+      user = User.new(name: 'name', email: '', password_digest: 'password')
+      expect(user).not_to be_valid
+    end
+  
+    it "パスワードが空ならバリデーションが通らない" do
+      user = User.new(name: 'name', email: 'a@example.com', password_digest: '')
+      expect(user).not_to be_valid
+    end
+  
+    it "titleとcontentに内容が記載されていればバリデーションが通る" do
+      user = User.new(name: 'name', email: 'a@example.com', password_digest: 'password')
+      expect(user).to be_valid
+    end
+  end
+
